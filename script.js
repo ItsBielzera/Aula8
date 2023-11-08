adcedf44f851471e2f85dc1fb94013e72edee235
Gabriel Durante <gabrielhdurante@iclou.com>
2023-11-08 14:48:44 -0300

    tudo ok

--- script.js
+++ script.js
@@ -1,43 +1,22 @@
-// Defina o construtor do objeto Empréstimo
-function Empréstimo(valor, parcelas, juros) {
-  this.valorEmprestimo = valor;
-  this.numParcelas = parcelas;
-  this.taxaJuros = juros;
-}
+function simularEmprestimo() {
+  var valorEmprestimo = parseFloat(document.getElementById("valorEmprestimo").value);
+  var numParcelas = parseInt(document.getElementById("numParcelas").value);
+  var taxaJuros = parseFloat(document.getElementById("taxaJuros").value);
 
-// Adicione um método para calcular as parcelas do empréstimo
-Empréstimo.prototype.calcularParcelas = function() {
-  var parcelas = [];
-  var valorRestante = this.valorEmprestimo;
-  for (var parcela = 1; parcela <= this.numParcelas; parcela++) {
-    var valorParcela = valorRestante * this.taxaJuros;
-    parcelas.push({
-      numero: parcela,
-      valor: valorParcela.toFixed(2),
-    });
-    valorRestante -= valorParcela;
+  if (isNaN(valorEmprestimo) || isNaN(numParcelas) || isNaN(taxaJuros)) {
+    document.getElementById("resultado").innerHTML = "Valores inválidos. Por favor, insira valores numéricos válidos.";
+    return;
   }
-  return parcelas;
-};
 
-// Crie uma função para iniciar o simulador
-function iniciarSimulador() {
-  var valor = parseFloat(prompt("Digite o valor do empréstimo:"));
-  var parcelas = parseInt(prompt("Digite o número de parcelas desejadas:"));
-  var juros = parseFloat(prompt("Digite a taxa de juros (em formato decimal, por exemplo, 0.05 para 5%):"));
+  var mensagem = "Simulador de Empréstimo\n\n";
+  var valorRestante = valorEmprestimo;
 
-  if (!isNaN(valor) && !isNaN(parcelas) && !isNaN(juros)) {
-    var emprestimo = new Empréstimo(valor, parcelas, juros);
-    var resultado = emprestimo.calcularParcelas();
-
-    var mensagem = "Simulador de Empréstimo\n\n";
-    for (var i = 0; i < resultado.length; i++) {
-      mensagem += "Parcela " + resultado[i].numero + ": R$" + resultado[i].valor + "\n";
-    }
-
-    mensagem += "\nEmpréstimo quitado!";
-    alert(mensagem);
-  } else {
-    alert("Valores inválidos. Por favor, insira valores numéricos válidos.");
+  for (var parcela = 1; parcela <= numParcelas; parcela++) {
+    var valorParcela = valorRestante * taxaJuros;
+    mensagem += "Parcela " + parcela + ": R$" + valorParcela.toFixed(2) + "<br>";
+    valorRestante -= valorParcela; // Reduz o valor do empréstimo
   }
+
+  mensagem += "<br>Empréstimo quitado!";
+  document.getElementById("resultado").innerHTML = mensagem;
 }
