55a2d96682e80cf04c204e345d0b1356369ac98a
Gabriel Durante <gabrielhdurante@iclou.com>
2023-11-08 14:32:45 -0300

    testando

--- script.js
+++ script.js
@@ -1,18 +1,46 @@
-var valorEmprestimo = parseFloat(prompt("Digite o valor do empréstimo:"));
-var numParcelas = parseInt(prompt("Digite o número de parcelas desejadas:"));
-var taxaJuros = parseFloat(prompt("Digite a taxa de juros (em formato decimal, por exemplo, 0.05 para 5%):"));
-
-if (!isNaN(valorEmprestimo) && !isNaN(numParcelas)) {
-  var mensagem = "Simulador de Empréstimo\n\n";
-
-  for (var parcela = 1; parcela <= numParcelas; parcela++) {
-    var valorParcela = valorEmprestimo * taxaJuros;
-    mensagem += "Parcela " + parcela + ": R$" + valorParcela.toFixed(2) + "\n";
-    valorEmprestimo -= valorParcela; // Reduz o valor do empréstimo
-  }
-
-  mensagem += "\nEmpréstimo quitado!";
-  alert(mensagem);
-} else {
-  alert("Valores inválidos. Por favor, insira valores numéricos válidos.");
+// Defina o construtor do objeto Empréstimo
+function Empréstimo(valor, parcelas, juros) {
+  this.valorEmprestimo = valor;
+  this.numParcelas = parcelas;
+  this.taxaJuros = juros;
 }
+
+// Adicione um método para calcular as parcelas do empréstimo
+Empréstimo.prototype.calcularParcelas = function() {
+  var parcelas = [];
+  var valorRestante = this.valorEmprestimo;
+  for (var parcela = 1; parcela <= this.numParcelas; parcela++) {
+    var valorParcela = valorRestante * this.taxaJuros;
+    parcelas.push({
+      numero: parcela,
+      valor: valorParcela.toFixed(2),
+    });
+    valorRestante -= valorParcela;
+  }
+  return parcelas;
+};
+
+// Crie uma função para iniciar o simulador
+function iniciarSimulador() {
+  var valor = parseFloat(prompt("Digite o valor do empréstimo:"));
+  var parcelas = parseInt(prompt("Digite o número de parcelas desejadas:"));
+  var juros = parseFloat(prompt("Digite a taxa de juros (em formato decimal, por exemplo, 0.05 para 5%):"));
+
+  if (!isNaN(valor) && !isNaN(parcelas) && !isNaN(juros)) {
+    var emprestimo = new Empréstimo(valor, parcelas, juros);
+    var resultado = emprestimo.calcularParcelas();
+
+    var mensagem = "Simulador de Empréstimo\n\n";
+    for (var i = 0; i < resultado.length; i++) {
+      mensagem += "Parcela " + resultado[i].numero + ": R$" + resultado[i].valor + "\n";
+    }
+
+    mensagem += "\nEmpréstimo quitado!";
+    alert(mensagem);
+  } else {
+    alert("Valores inválidos. Por favor, insira valores numéricos válidos.");
+  }
+}
+
+// Agora, você pode invocar o simulador quando o usuário realizar alguma ação, por exemplo:
+document.getElementById("simularBtn").addEventListener("click", iniciarSimulador);
